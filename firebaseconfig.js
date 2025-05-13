f89cd63acca8de8a9bd0f10e9c42c03b3f065744
Anointeddrew <abah.d.andrew@gmail.com>
2025-05-13 20:57:19 +0100

    Added Landing page and initial signin and signup

---
+++ firebaseconfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCmADWJoJrg2e4k9gjupSBbKRi17Ov8frY",
+  authDomain: "quicknotes-1da62.firebaseapp.com",
+  projectId: "quicknotes-1da62",
+  storageBucket: "quicknotes-1da62.firebasestorage.app",
+  messagingSenderId: "1068832959826",
+  appId: "1:1068832959826:web:e33b5361e245b32b361e8b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
