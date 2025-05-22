62291c8ea2237d2e24012fee5181a2a6c0752f17
Anointeddrew <abah.d.andrew@gmail.com>
2025-05-22 10:10:13 +0100

    Added Authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,47 @@
-import Landing from "./routes/Landing";
+import Landing from "./routes/landing";
 import {Routes, Route, Navigate} from 'react-router-dom';
 import Login from "./routes/Login";
 import Signup from "./routes/Signup";
 import Navbar from "./routes/Components/Navbar";
+import {useAuth} from "./context/AuthContext";
+import ProtectedRoute from "./routes/Components/ProtectedRoute";
+import Dashboard from "./routes/Dashboard";
+
 
 function App() {
+  const {loading, currentUser} = useAuth();
+
+  if (loading) {
+    return (<div className="min-h-screen bg-slate-50 flex items-center justify-center">
+      <div className="animate-pulse text-indigo-600 text-xl">Loading Quicknotes</div>
+    </div>)
+  }
+
+
   return (
     <>
     <div className="min-h-screen bg-slate-50">
     <Navbar/>
       <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
+        <Route path="/" element={ currentUser ? <Navigate to = "/dashboard" replace /> :  
+          <Landing />
+        }
+        />
+        <Route path="/login" element={ currentUser ? <Navigate to = "/dashboard" replace /> : 
+          <Login />
+        }
+        />
+        <Route path="/signup" element={ currentUser ? <Navigate to="/dashboard" replace /> :  
+          <Signup />
+        }
+        /> 
+        <Route path="/dashboard" element={ 
+          <ProtectedRoute>
+          <div className="container mx-auto px-4 py-8">
+            <Dashboard />
+          </div>
+        </ProtectedRoute>}
+        />
       </Routes>
     </div>
    </> 
--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,10 +1,37 @@
 import React, { useState } from 'react';
 import { Notebook } from 'lucide-react';
-import { Link } from 'react-router';
+import { Link, useNavigate } from 'react-router';
+import { useAuth } from "../context/AuthContext";
 
 function Login() {
     const [email, setEmail] = useState();
     const [password, setPassword] = useState();
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState("");
+    const {login} = useAuth();
+    const navigate = useNavigate();
+
+     const handleSubmit = async (e) => {
+        e.PreventDefault();
+        setError("");
+
+        if (!email || !password) {
+            return setError("Please fill in all fields")
+        }
+
+         
+
+        try {
+            setLoading(true);
+            await login(email, password);
+            navigate("/dashboard");
+        } catch (err) {
+            setError('Failed to create account: ' + (err.message || 'Please try again'))
+        } finally {
+            setLoading(false)
+        }
+    }
+
     return (
         <div className="max-w-md mx-auto mt-10">
             <div className="bg-white rounded-lg shadow-md p-8">
@@ -14,7 +41,15 @@ function Login() {
                     <p className="text-gray-600">Sign in to access your notes</p>
                     </div>
 
-                    <form>
+                    {
+                        error && (
+                            <div className="bg-red-50 text-red-700 p-3 rounded-md mb-4 text-sm">
+                                {error}{""}
+                            </div>
+                        )
+                    }
+
+                    <form onSubmit={handleSubmit}>
                         <div className="mb-4">
                             <label htmlFor="email"
                              className="block text-sm font-medium text-gray-700 mb-1">
@@ -24,6 +59,7 @@ function Login() {
                             id="email"
                             type="email"
                             value={email}
+                            onChange={(e)=> setEmail(e.target.value)}
                             className="w-full px-3 py-2 border border-grey-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
                             placeholder="you@example.com" 
                             required 
@@ -37,13 +73,15 @@ function Login() {
                             id="password"
                             type="password"
                             value={password}
+                            onChange={(e)=> setPassword(e.target.value)}
                             className="w-full px-3 py-2 border border-grey-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
                             placeholder="********" 
                             required 
                           />
                         </div>
-                        <button className="w-full bg-indigo-600 text-white py-2 rounded-md hover:bg-indigo-700 transition-colors focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-50 disabled:cursor-not-allowed mt-4" type="submit">
-                            Sign in
+                        <button className="w-full bg-indigo-600 text-white py-2 rounded-md hover:bg-indigo-700 transition-colors focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-50 disabled:cursor-not-allowed mt-4" type="submit"
+                        diabled={loading}>
+                            {loading ? "Signing in..." : "Sign In"}
                         </button>
                     </form>
 
